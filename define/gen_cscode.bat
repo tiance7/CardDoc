a35439ade1ed76a62e333df59b41f165c1effa9b
isengardd <xuyao8598@msn.com>
2018-03-20 23:17:29 +0800

    1.卡牌效果表和卡牌表结构修改 2.增加环境变量XIAN_CARD_PATH，指定Unity工程路径

--- define/gen_cscode.bat
+++ define/gen_cscode.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 set TOOLSDIR=..\sixcube\tools\templatec
-set TARGETDIR=D:\unityProject\XianCard\Assets\Scripts\Templates
+set TARGETDIR=%XIAN_CARD_PATH%\Assets\Scripts\Templates
 
-svn up
+rem svn up
 
 @echo "Generate csharp template files begin"
 del /s %TARGETDIR%\*.cs
 python %TOOLSDIR%\template_to_csharp.py -i template -o %TARGETDIR%\
-svn add %TARGETDIR%\*.cs --force
+rem svn add %TARGETDIR%\*.cs --force
 @echo "Generate csharp template files end."
 pause
